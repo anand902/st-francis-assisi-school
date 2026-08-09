84bfec41300708562faed0034d3258f57ada30a7
philip horo <56852632+anand902@users.noreply.github.com>
2026-08-09 06:11:07 +0530

    Add Firebase configuration to firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBs5Nxm8PZvsscChRMd8REat3hJat1mC9Q",
+  authDomain: "school-management-21fc6.firebaseapp.com",
+  projectId: "school-management-21fc6",
+  storageBucket: "school-management-21fc6.firebasestorage.app",
+  messagingSenderId: "168711602529",
+  appId: "1:168711602529:web:ac5170d6cd429f64040520"
+};
